0651c2953644ac0a7b2fe4cc4b9f97753b4e8654
Nonus10 <luka.krstik@protonmail.com>
2021-09-06 22:00:24 +0200

    photo collection added changed sranja dovolno kazano

--- js/pcms.js
+++ js/pcms.js
@@ -48,7 +48,7 @@ function loadPage(page, pageElement) {
 
 	// Load the page
 
-	img.attr('src', 'uc1/' +  page + '.jpg');
+	img.attr('src', 'pcms/' +  page + '.jpg');
 
 	loadRegions(page, pageElement);
 
@@ -193,7 +193,7 @@ function loadSmallPage(page, pageElement) {
 	img.unbind('load');
 	// Loadnew page
 
-	img.attr('src', 'uc1/' +  page + '.jpg');
+	img.attr('src', 'pcms/' +  page + '.jpg');
 }
 
 // http://code.google.com/p/chromium/issues/detail?id=128488
