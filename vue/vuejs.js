171e56d2953add1c47e7d4d5de35f63d6b286e3f
Luka Krstikj <luka.krstik@protonmail.com>
2021-12-23 00:04:19 +0100

    smenet indeks :)

--- vue/vuejs.js
+++ vue/vuejs.js
@@ -3,7 +3,7 @@ const app = Vue.createApp({})
 app.component('app-nav', {
   template: `
            <a class="navbar-brand" href="https://lukakrstik.com">
-                <img src="https://raw.githubusercontent.com/Nonus10/websajt/master/images/logo.svg"
+                <img src="images/logo.svg"
                 style="width: 4.267rem; height: auto"
                 alt="Luka Krstik">
            </a>
@@ -34,7 +34,32 @@ var msnry;
   });
 });
 
-
+// Navbar za SK
+const appNAV = Vue.createApp({})
+appNAV.component('app-nav2', {
+    template: `
+           <a class="navbar-brand" href="https://lukakrstik.com">
+                <img src="images/logoW.svg" class="logoSK"
+                style="width: 4.267rem; height: auto;"
+                alt="Luka Krstik">
+           </a>
+           <ul class="navbar-nav float-right mr-1" style="text-align: right;" id="navigation-app2">
+                        <li class="nav-item">
+                           <a href="https://lukakrstik.com" class="header-info hyperlink my-0" style="color: white">Home</a>
+                        </li>
+                        <li class="nav-item">
+                           <a href="https://lukakrstik.com/gallery" class="header-info hyperlink my-0" style="color: white">Gallery</a>
+                        </li>
+                        <li class="nav-item">
+                           <a href="https://lukakrstik.com/albums" class="header-info hyperlink my-0" style="color: white">Albums</a>
+                        </li>
+                        <li class="nav-item">
+                           <a href="https://lukakrstik.com/contact" class="header-info hyperlink my-0" style="color: white">Contact</a>
+                        </li>
+           </ul>
+            `
+})
+appNAV.mount('#navigation-app2')
 
 
       
